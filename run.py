09664515058bc5d6fa8e9cb221b089557f3e4bed
Vera Leitao <veraleitao.dev@gmail.com>
2020-10-06 16:56:29 +0000

    added method POST to both contact.html and run.py

--- run.py
+++ run.py
@@ -34,7 +34,7 @@ def about_member(member_name):
     return render_template("member.html", member=member)
 
 
-@app.route("/contact")
+@app.route("/contact", methods=["GET", "POST"])
 def contact():
     return render_template("contact.html", page_title="Contact")
 
