a2c57d8b39e4b4c5386bc9244bde1182e6956e43
Vera Leitao <veraleitao.dev@gmail.com>
2020-10-07 20:43:47 +0000

    added feedback to contact form

--- run.py
+++ run.py
@@ -1,9 +1,9 @@
 import os
 import json
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash
 
 app = Flask(__name__)
-
+app.secret_key = "some_secret"
 
 @app.route("/")
 def index():
@@ -36,6 +36,9 @@ def about_member(member_name):
 
 @app.route("/contact", methods=["GET", "POST"])
 def contact():
+    if request.method == "POST":
+        flash("Thanks {}, we have recieved your message!".format(
+              request.form["name"]))
     return render_template("contact.html", page_title="Contact")
 
 
